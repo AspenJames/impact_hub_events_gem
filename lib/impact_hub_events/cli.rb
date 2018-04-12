171764e462515eadc56ae3bdac3efeeb731a8f80
Aspen James <aspenjames@tqca.org>
2018-04-11 19:55:27 -0700

    Added #call, init #list_menu

--- lib/impact_hub_events/cli.rb
+++ lib/impact_hub_events/cli.rb
@@ -1,5 +1,11 @@
 class ImpactHubEvents::CLI
   def call
-    
+    scraper = ImpactHubEvents::Scraper.new(https://impacthubseattle.com/events/)
+    scraper.scrape_events
+    self.list_menu
+  end
+
+  def list_menu
+
   end
 end
