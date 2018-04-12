90f543fc39838a7597d85c0463474e63b1051ede
Aspen <aspenjames@tqca.org>
2018-04-11 20:32:37 -0700

    Fixed syntax error

--- lib/impact_hub_events/cli.rb
+++ lib/impact_hub_events/cli.rb
@@ -1,6 +1,6 @@
 class ImpactHubEvents::CLI
   def call
-    scraper = ImpactHubEvents::Scraper.new(https://impacthubseattle.com/events/)
+    scraper = ImpactHubEvents::Scraper.new("https://impacthubseattle.com/events/")
     scraper.scrape_events
     self.list_menu
   end
