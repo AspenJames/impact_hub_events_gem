3def85f9fe96bb0981dfe4a72c70c4de71840580
Aspen <aspenjames@tqca.org>
2018-04-14 11:20:37 -0700

    Added argument to initialize, added @@all reader

--- lib/impact_hub_events/event.rb
+++ lib/impact_hub_events/event.rb
@@ -1,8 +1,14 @@
 class ImpactHubEvents::Event
-  attr_accessor :date, :time, :title, :description, :location 
+  attr_accessor :url, :date, :time, :title, :description, :location
 
-  def initialize
+  def initialize(url)
+    @url = url
     @@all = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 
