e7e5a29b7772ad1d8cdd475a4b655731b7932565
Aspen <aspenjames@tqca.org>
2018-04-14 11:24:43 -0700

    Added methods to create and populate events

--- lib/impact_hub_events/scraper.rb
+++ lib/impact_hub_events/scraper.rb
@@ -7,8 +7,30 @@ class ImpactHubEvents::Scraper
     @doc = Nokogiri::HTML(open(url))
   end
 
-  def scrape_events
+  def create_events
+    @doc.css("div.pbr-event-schedule div.row div.col-sm-10 article div.right h3 a").map{|link| ImpactHubEvents::Event.new(link.attribute("href").value)}
 
+    #event link @doc.css("div.pbr-event-schedule div.row div.col-sm-10 article div.right h3 a").map{|link| link.attribute("href").value}
+
+
+    #---- From inidividual page
+
+    #date doc.css("div.col-sm-3 ul li span").first.text
+    #time doc.css("div.col-sm-3 ul li span")[1].text
+    #title doc.css("div.col-sm-9 h1").text
+    #location doc.css("div.col-sm-3 ul li span")[3].text
+    #description doc.css("div.col-sm-9 p").first.text
+  end
+
+  def populate_events
+    ImpactHubEvents::Event.all.each do |event|
+      event_detail = Nokogiri::HTML(open(event.url))
+      event.date = event_detail.css("div.col-sm-3 ul li span").first.text
+      event.time = event_detail.css("div.col-sm-3 ul li span")[1].text
+      event.title = event_detail.css("div.col-sm-9 h1").text
+      event.location = event_detail.css("div.col-sm-3 ul li span")[3].text
+      event.description = event_detail.css("div.col-sm-9 p").first.text
+    end
   end
 
 end
