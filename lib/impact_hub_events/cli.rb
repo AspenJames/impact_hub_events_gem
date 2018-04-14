30f3a7dc463dcf539cc8008ed8f044c92e4a6690
Aspen <aspenjames@tqca.org>
2018-04-14 12:16:10 -0700

    Added #list_events, #list_menu

--- lib/impact_hub_events/cli.rb
+++ lib/impact_hub_events/cli.rb
@@ -1,11 +1,21 @@
 class ImpactHubEvents::CLI
   def call
+    puts "Upcoming events at Impact Hub Seattle:"
     scraper = ImpactHubEvents::Scraper.new("https://impacthubseattle.com/events/")
-    scraper.scrape_events
-    self.list_menu
+    scraper.create_events
+    scraper.populate_events
+    self.list_events
+  end
+
+  def list_events
+    ImpactHubEvents::Event.all.each_with_index do |event, index|
+      puts "#{index + 1}. #{event.title}"
+    end
+
   end
 
   def list_menu
+    puts "Select an event for more info or \"exit!\" to quit."
 
   end
 end
