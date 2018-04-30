894f4b5e0b592547ba37d47c56d2d57629fdd235
Aspen James <aspenjames@tqca.org>
2018-04-30 02:22:56 -0700

    fixed #create_events to include multiple events per day, and fixed #populate_events to handle possible missing info

--- lib/impact_hub_events/scraper.rb
+++ lib/impact_hub_events/scraper.rb
@@ -9,8 +9,11 @@ class ImpactHubEvents::Scraper
   end
 
   def create_events
-    @doc.css("div.pbr-event-schedule div.row").map do |row|
-      ImpactHubEvents::Event.new(row.css("div.col-sm-10 div article div.right a").attribute("href").value)
+    @doc.css("div.pbr-event-schedule div.row").each do |row|
+      row.css("div.col-sm-10 div.session-item-content-wrapper").each do |event|
+        # binding.pry
+        ImpactHubEvents::Event.new(event.css("article div.right a").attribute("href").value)
+      end
     end
     #---- From inidividual page
 
@@ -24,11 +27,11 @@ class ImpactHubEvents::Scraper
   def populate_events
     ImpactHubEvents::Event.all.each do |event|
       event_detail = Nokogiri::HTML(open(event.url))
-      event.date = event_detail.css("div.col-sm-3 ul li span").first.text
-      event.time = event_detail.css("div.col-sm-3 ul li span")[1].text
-      event.title = event_detail.css("div.col-sm-9 h1").text
-      event.location = event_detail.css("div.col-sm-3 ul li span")[3].text
-      event.description = event_detail.css("div.col-sm-9 p").first.text
+      event.date = event_detail.css("div.col-sm-3 ul li span").first.text unless (event_detail.css("div.col-sm-3 ul li span").first).nil?
+      event.time = event_detail.css("div.col-sm-3 ul li span")[1].text unless (event_detail.css("div.col-sm-3 ul li span")[1]).nil?
+      event.title = event_detail.css("div.col-sm-9 h1").text unless (event_detail.css("div.col-sm-9 h1")).nil?
+      event.location = event_detail.css("div.col-sm-3 ul li span")[3].text unless (event_detail.css("div.col-sm-3 ul li span")[3]).nil?
+      event.description = event_detail.css("div.col-sm-9 p").first.text unless (event_detail.css("div.col-sm-9 p").first).nil?
     end
   end
 
